258e62142ec4f9154745c835a50c86b492fff5dc
uri752 <uri752blag@mail.ru>
2023-12-02 17:00:25 +0300

    fix error

--- page3.php
+++ page3.php
@@ -4,12 +4,13 @@ session_start();
 
 echo 'Page 3 <br/>';
 
-$_SESSION['count'] = isset($_SESSION['count']) ? ++$_SESSION['count'] : 0;
+// Доработка - человек уже перешел на страницу, следовательно счетчик = 1
+$_SESSION['count'] = isset($_SESSION['count']) ? ++$_SESSION['count'] : 1;
 
 echo 'Количество открытий: '.$_SESSION['count'];
 
 $count = $_SESSION['count'];
-if ($count % 3 === 0) {
-    $_SESSION['page3'] = $count;
+if ($count % 3 === 0) {    
+    // Доработка - убрал лишнюю переменню для page3 и использую count
     header('Location: http://localhost:8000/page4.php');
 }
